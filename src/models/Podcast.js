5e1487ee11fb6234f936ec6ef1ec154fb59af044
Nick Alberts <nwalberts5@gmail.com>
2023-12-21 17:08:52 -0500

    43

--- src/models/Podcast.js
+++ src/models/Podcast.js
@@ -5,18 +5,29 @@ const podcastsPath = "podcasts.json"
 // const podcastObjects = JSON.parse(fs.readFileSync(podcastsPath)).podcasts
 
 class Podcast {
-  constructor({ title, description, network }) {
+  constructor({ title, description = "NA", network = "NA" }) {
     this.title = title
-    this.description = description || "NA"
-    this.network = network || "NA"
+    this.description = description 
+    this.network = network 
   }
 
   static findAll() {
-
+    const podcastData = JSON.parse(fs.readFileSync(podcastsPath))
+    const podcastsArray = podcastData.podcasts.map(podcastObject => {
+      return new Podcast(podcastObject)
+    })
+    return podcastsArray
   }
 
   save() {
-
+    if (this.title.trim() !== "") {
+      const podcasts = this.constructor.findAll()
+      podcasts.push(this)
+      fs.writeFileSync(podcastsPath, JSON.stringify({podcasts: podcasts}))
+      return true
+    } else {
+      return false
+    }
   }
 }
 
