d93d5e7a30b0b9607a0cf1b77a16d0677bb2b55f
Nick Alberts <nwalberts5@gmail.com>
2024-03-28 13:43:25 -0400

    43

--- src/models/Podcast.js
+++ src/models/Podcast.js
@@ -5,29 +5,18 @@ const podcastsPath = "podcasts.json"
 // const podcastObjects = JSON.parse(fs.readFileSync(podcastsPath)).podcasts
 
 class Podcast {
-  constructor({ title, description = "NA", network = "NA" }) {
+  constructor({ title, description, network }) {
     this.title = title
     this.description = description 
     this.network = network 
   }
 
   static findAll() {
-    const podcastData = JSON.parse(fs.readFileSync(podcastsPath))
-    const podcastsArray = podcastData.podcasts.map(podcastObject => {
-      return new Podcast(podcastObject)
-    })
-    return podcastsArray
+
   }
 
   save() {
-    if (this.title.trim() !== "") {
-      const podcasts = this.constructor.findAll()
-      podcasts.push(this)
-      fs.writeFileSync(podcastsPath, JSON.stringify({podcasts: podcasts}))
-      return true
-    } else {
-      return false
-    }
+
   }
 }
 
--- src/routes/podcastsRouter.js
+++ src/routes/podcastsRouter.js
@@ -11,28 +11,9 @@ podcastsRouter.get("/", (req, res) => {
 })
 
 // new route 
-podcastsRouter.get("/new", (req, res) => {
-  res.render("podcasts/new")
-})
+
 
 // POST route
-podcastsRouter.post("/ILickedEverything", (req, res) => {
-  const podcast = new Podcast(req.body)
-
-  if (podcast.save() === true)  {
-    res.redirect("/podcasts")
-  } else {
-    res.render("podcasts/new")
-  }
-})
-
-// https://upload.wikimedia.org/wikipedia/en/thumb/7/73/Pikachu_artwork_for_Pok%C3%A9mon_Red_and_Blue.webp/220px-Pikachu_artwork_for_Pok%C3%A9mon_Red_and_Blue.webp.png
-// retrieve data from params
-
-// persist the new record 
-
-// determine whether we want to re-render or redirect
-
 
 
 export default podcastsRouter
